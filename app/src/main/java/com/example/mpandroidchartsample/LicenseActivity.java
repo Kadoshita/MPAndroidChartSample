50d0a6d7bb0f8d0bd67da80e5b8b1475ad757881
kadoshita <kadoshita.inct@gmail.com>
2016-08-07 18:43:12 +0900

    open Github & License URLs

--- app/src/main/java/com/example/mpandroidchartsample/LicenseActivity.java
+++ app/src/main/java/com/example/mpandroidchartsample/LicenseActivity.java
@@ -1,13 +1,41 @@
 package com.example.mpandroidchartsample;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class LicenseActivity extends AppCompatActivity {
 
+    private TextView githuburl,licenseurl;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_license);
+
+        githuburl=(TextView)findViewById(R.id.textView);
+        licenseurl=(TextView)findViewById(R.id.textView2);
+
+        githuburl.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri=Uri.parse("https://github.com/PhilJay/MPAndroidChart");
+                Intent intent=new Intent(Intent.ACTION_VIEW,uri);
+                startActivity(intent);
+            }
+        });
+
+        licenseurl.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri=Uri.parse("http://www.apache.org/licenses/LICENSE-2.0");
+                Intent intent=new Intent(Intent.ACTION_VIEW,uri);
+                startActivity(intent);
+            }
+        });
     }
 }
